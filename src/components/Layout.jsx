bf6e8c4e2694935613fbae6bdf16b695f71e07c5
Dev <dev@reloncavi.cl>
2026-09-08 12:50:26 -0300

    fix: make main flex column so contacto gradient fills to footer (no white bar)

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,7 +7,7 @@ export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-1">
+      <main className="flex-1 flex flex-col">
         <Outlet />
       </main>
       <Footer />
