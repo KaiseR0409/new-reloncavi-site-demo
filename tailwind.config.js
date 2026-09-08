708c767c536b06f02d7d46a9cfe48fb191be72ef
Dev <dev@reloncavi.cl>
2026-09-08 10:18:33 -0300

    chore: scaffold Vite React Tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        lust: "#b3123f",
+        "white-2": "#f2f2f2",
+        isabelline: "#f4f0ec",
+        "outer-space": "#333c45",
+        "selective-yellow": "#ffb400",
+      },
+      fontFamily: {
+        sans: ["Lato", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
