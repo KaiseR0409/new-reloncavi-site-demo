8983220957d4eec45f0a86fe98962551478d245b
Dev <dev@reloncavi.cl>
2026-09-08 12:51:15 -0300

    feat: no footer on contacto page, gradient fills viewport to bottom edge

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,18 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 import ScrollTop from "./ScrollTop"
 
 export default function Layout() {
+  const { pathname } = useLocation()
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-1 flex flex-col">
         <Outlet />
       </main>
-      <Footer />
+      {pathname !== "/contacto" && <Footer />}
       <ScrollTop />
     </div>
   )
